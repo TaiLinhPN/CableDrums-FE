b1c1755ae738984ff7dff779150b616540a54ba9
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2023-05-24 15:33:55 +0700

    feat: update login ui

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,6 +4,9 @@ import LoginForm from "../containers/LoginFormContainer";
 const LoginPage = () => {
   return (
     <div className="login-body">
+      <p className="company-name">
+        <span>EnergySure tech</span> Energy Corporation
+      </p>
       <LoginForm />
     </div>
   );
