18d68a05c64f48f105540433222234acff4bec21
Linh Nguyen Bui Tai <linh.nguyenbuitai>
2023-05-31 15:45:11 +0700

    update to build

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const URL =
-  process.env.VITE_REACT_APP_API_SOCKET_IO_URL || "ttp://3.25.82.146:1234";
+  process.env.VITE_REACT_APP_API_SOCKET_IO_URL || "http://3.25.82.146:1234";
 
 export const socket = io(URL);
