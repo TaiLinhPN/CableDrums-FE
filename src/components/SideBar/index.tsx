808790dcc951cacb6487c22437e1a6356d61581e
Linh Nguyen Bui Tai <linh.nguyenbuitai>
2023-05-31 15:47:27 +0700

    production 1.0

--- src/components/SideBar/index.tsx
+++
@@ -1,8 +0,0 @@
-
-const SitaBar = () => {
-  return (
-    <div className="bg-gray-200">SitaBar</div>
-  )
-}
-
-export default SitaBar
--- src/utils/socket.ts
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-
-const URL =
-  process.env.VITE_REACT_APP_API_SOCKET_IO_URL || "http://3.25.82.146:1234";
-
-export const socket = io(URL);
